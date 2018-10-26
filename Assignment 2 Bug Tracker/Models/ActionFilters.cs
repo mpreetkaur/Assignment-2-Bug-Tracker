fb7afbdca2e6d196a22f8052a84953b9388a8245
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-10-26 11:15:55 -0500

    done with part 1

--- Assignment 2 Bug Tracker/Models/ActionFilters.cs
+++ Assignment 2 Bug Tracker/Models/ActionFilters.cs
@@ -8,21 +8,13 @@ namespace Assignment_2_Bug_Tracker.Models
 {
     public class ActionFilters : ActionFilterAttribute
     {
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            if (HttpContext.Request.Browser.Crawler)
-            {
-                filterContext.Result = new EmptyResult();
-            }
-            else
-            {
-            base.OnActionExecuted(filterContext);
-            }
-        }
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            base.OnActionExecuting(filterContext);
+            if (filterContext.HttpContext.Request.Browser.Crawler)
+            {
+                filterContext.Result = new EmptyResult();
+            }
         }
     }
 }
