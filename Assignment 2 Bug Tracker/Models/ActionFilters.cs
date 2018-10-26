006ccbc596e64cc91260537a17df04e43d8ea3ec
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-10-26 11:11:14 -0500

    done part 2,3,4

---
+++ Assignment 2 Bug Tracker/Models/ActionFilters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_2_Bug_Tracker.Models
+{
+    public class ActionFilters : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (HttpContext.Request.Browser.Crawler)
+            {
+                filterContext.Result = new EmptyResult();
+            }
+            else
+            {
+            base.OnActionExecuted(filterContext);
+            }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
---
+++ Assignment 2 Bug Tracker/Models/ExtensionMethod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_2_Bug_Tracker.Models
+{
+    public static class ExtensionMethod
+    {
+        public static string ToDigitsOnly(this string str)
+        {
+            var myString = "";
+            foreach (var e in str)
+            {
+                if (char.IsDigit(e))
+                {
+                    myString += e;
+                }
+            }
+            return myString;
+        }
+
+        public static int ToAge(this DateTime dateOfBirth)
+        {
+            int age = (Int32.Parse(DateTime.Today.ToString("yyyyMMdd")) - Int32.Parse(dateOfBirth.ToString("yyyyMMdd"))) / 10000;
+
+            return age;
+        }
+    }
+}
