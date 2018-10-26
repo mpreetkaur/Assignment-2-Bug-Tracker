5ff544943c2ec6a6f23be881221b6685ffd26341
Manpreet Kaur <mpreet2000kaur@gmail.com>
2018-10-26 11:27:09 -0500

    final commit

--- Assignment 2 Bug Tracker/Controllers/HomeController.cs
+++ Assignment 2 Bug Tracker/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assignment_2_Bug_Tracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace Assignment_2_Bug_Tracker.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
